2fe78068b07643e63e4b259a3e5fa37fe8043511
Josh Schoenecker <schoeneckerj@gmail.com>
2020-04-16 20:26:07 -0600

    and search for jokes framing added

---
+++ clientsrc/src/components/SearchJokes.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="component">
+
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'SearchJokes',
+    data(){
+        return {}
+    },
+    computed:{},
+    methods:{},
+    components:{}
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ clientsrc/src/views/SearchForJokes.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="component">
+
+
+    </div>
+</template>
+
+
+<script>
+import SearchJokes from "../components/SearchJokes"
+export default {
+    name: 'SearchForJokes',
+    data(){
+        return {}
+    },
+    computed:{},
+    methods:{},
+    components:{ SearchJokes }
+}
+</script>
+
+
+<style scoped>
+
+</style>
